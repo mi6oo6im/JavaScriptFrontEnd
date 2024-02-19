48c59ba88b778b3e55c5f5421d5557814feb01e4
Mihail Istiliyanov <54684159+mi6oo6im@users.noreply.github.com>
2024-02-19 20:33:43 +0000

    demo

---
+++ T01_SyntaxFundamentals/demo.js
@@ -0,0 +1,12 @@
+let a = 10;
+let b = 15;
+
+function test(x, y) {
+    if (x >= y) {
+        console.log(`${x} is larger or equal than ${y}`);
+    } else {
+        console.log(`${y} is larger than ${x}`);
+    }
+}
+
+test(a , b)
