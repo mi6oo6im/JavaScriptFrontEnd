e61beeb6026a1be555139d21f1af2d8fde300d31
mistiliyanov <mistiliyanov@salesforce.com>
2024-03-11 21:53:47 +0000

    lab exercise 1

---
+++ T07_ObjectsAndClasses/P01/personInfo.js
@@ -0,0 +1,15 @@
+function solve(firstName, lastName, age) {
+    const person = {
+        firstName,
+        lastName,
+        age,
+    }
+
+    return person;
+
+}
+
+solve("Peter", "Pan", "20");
+console.log('-----------');
+solve("George", "Smith", "18");
+console.log('-----------');
