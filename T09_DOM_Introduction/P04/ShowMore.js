23b5b7b07ff4845d27c95a39c907bec050ca03eb
Mihail Istiliyanov <54684159+mi6oo6im@users.noreply.github.com>
2024-03-23 19:23:58 +0000

    exercise lab 2

--- T09_DOM_Introduction/P04/ShowMore.js
+++ T09_DOM_Introduction/P04/ShowMore.js
@@ -1,3 +1,6 @@
 function showText() {
-    // TODO
+    const readMoreElement = document.getElementById('more');
+    const hiddenElement = document.getElementById('text');
+    readMoreElement.style.display = 'none';
+    hiddenElement.style.display = 'inline';
 }
