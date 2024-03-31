a01067e5172d9589decdd2080aa03fc5615904a6
Mihail Istiliyanov <54684159+mi6oo6im@users.noreply.github.com>
2024-03-31 14:48:16 +0100

    lag exercises 3

--- T11_DOM_ManipulationAndEvents/05. Highlight-Active/app.js
+++ T11_DOM_ManipulationAndEvents/05. Highlight-Active/app.js
@@ -1,3 +1,11 @@
 function focused() {
-    console.log('TODO:...');
+    const sectionsElements = Array.from(document.querySelectorAll('div > div'));
+    for (const element of sectionsElements) {
+        element.children[1].addEventListener('focus', () => {
+            element.classList.add('focused');
+        })
+        element.children[1].addEventListener('blur', () => {
+            element.classList.remove('focused');
+        })
+    }
 }
