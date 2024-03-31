159cef4fa99a10ae81038de82965440072be30e7
Mihail Istiliyanov <54684159+mi6oo6im@users.noreply.github.com>
2024-03-31 13:32:20 +0100

    exercise lab 2

--- T11_DOM_ManipulationAndEvents/03. Add-Delete/app.js
+++ T11_DOM_ManipulationAndEvents/03. Add-Delete/app.js
@@ -1,3 +1,21 @@
 function addItem() {
-    //TODO...
+    const inputElement = document.getElementById('newItemText');
+    const itemsListElement = document.getElementById('items');
+    // create new element and assign value
+    const newItemElement = document.createElement('li');
+    newItemElement.textContent = inputElement.value;
+    // create anchor tag and assign value
+    const deleteLinkElement = document.createElement('a');
+    deleteLinkElement.textContent = '[Delete]';
+    deleteLinkElement.href = '#';
+    // append link to list element
+    newItemElement.appendChild(deleteLinkElement);
+    // append list element to unordered list
+    itemsListElement.appendChild(newItemElement);
+    // add event handler
+    deleteLinkElement.addEventListener('click', () => {
+        newItemElement.remove()
+    })
+    // clear input element value
+    inputElement.value = '';
 }
